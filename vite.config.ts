2927aad70fd0e561b54d21486d9da323b88bbc49
consor92 <consor_92@hotmail.com>
2025-09-06 00:06:16 -0300

    qr

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,16 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0',  // permite conexiones externas
+    port: 4000,
+    allowedHosts: [
+      'escuelatecnica32de14.edu.ar',
+      'www.escuelatecnica32de14.edu.ar'
+    ],
+    hmr: {
+      host: 'escuelatecnica32de14.edu.ar',  // o 'www.escuelatecnica32de14.edu.ar', funciona con cualquiera
+      protocol: 'wss'
+    }
+  }
+});
