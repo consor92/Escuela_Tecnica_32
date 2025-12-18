8191484e38ff849bd8f6d24ad0f5d1298366f2ce
Gonzalo Consorti <gonzalo.consorti@bue.edu.ar>
2025-12-18 20:26:57 -0300

    Add files via upload

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['source.unsplash.com', 'cdn.pixabay.com'],
-  }
+  },
+  // Agregá esto aquí abajo:
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
