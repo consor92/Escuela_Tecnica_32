d10de1c9bf038f1bc2ac6115fa1ab3e75468fa60
Gonzalo Consorti <gonzalo.consorti@bue.edu.ar>
2026-05-05 23:06:40 -0300

    Update item.js

--- frontend/src/pages/api/item.js
+++ frontend/src/pages/api/item.js
@@ -1,6 +1,11 @@
 import { readData } from '../../lib/dataLoader';
 
 export default function handler(req, res) {
+    // Evitar caché
+    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', '0');
+    
     const data = readData('disciplines.json');
     res.status(200).json(data);
 }
