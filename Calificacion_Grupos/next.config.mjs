fc2d6cf566fd6329382d8439e29c57d531a4ff9b
Gonzalo Consorti <gonzalo.consorti@bue.edu.ar>
2026-05-12 21:40:41 -0300

    Update next.config.mjs

--- Calificacion_Grupos/next.config.mjs
+++ Calificacion_Grupos/next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
-    basePath: '/encuesta',
-    assetPrefix: '/encuesta',
 };
 
 export default nextConfig;
